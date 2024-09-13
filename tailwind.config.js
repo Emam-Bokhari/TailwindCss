7ae047e3485c2197fc4c7a0dd7c3b42c360d9dde
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-13 15:59:58 +0600

    style(custom font size): implemented custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: ["*"],
   theme: {
-    fontFamily:{
-      primary:'poppins',
-      secondary:"montserrat"
+    fontFamily: {
+      primary: "poppins",
+      secondary: "montserrat",
+    },
+    fontSize: {
+      primary: "40px",
+      secondary: "20px",
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
